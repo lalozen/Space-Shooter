6b472d8c2dfd4d2425d35f507e2f613f95de9219
lalozen <valisabenjalin002@gmail.com>
2024-06-29 00:30:27 +0530

    added movements to the playership

--- code/main.py
+++ code/main.py
@@ -13,19 +13,26 @@ surf = pygame.Surface((100, 100))
 surf.fill((255, 0, 0))
 
 player_surf = pygame.image.load('images/player.png').convert_alpha()
+player_rect = player_surf.get_rect(center=(width/2, height/2))
 star_surf = pygame.image.load('images/star.png').convert_alpha()
 
 star_positions = [(random.randint(0, width), random.randint(0, height)) for _ in range(20)]
 
+
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:b
+        if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
     display_surface.fill(('darkgray'))
     
     for star_position in star_positions:
         display_surface.blit(star_surf, star_position)
-    display_surface.blit(player_surf, (100, 100))
+    if player_rect.right < width:
+        player_rect.right += 1
+    else:
+        player_rect.left = 0
+    
+    display_surface.blit(player_surf, player_rect)
     
     pygame.display.update()
